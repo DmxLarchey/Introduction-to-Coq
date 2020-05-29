347f8e688b452a48c0ba8c17eb8b4a789fea40dc
Dominique Larchey-Wendling <larchey@gmail.com>
2020-05-29 16:06:55 +0200

    partially completed lab4_perm.v

--- lab4_perm.v
+++ lab4_perm.v
@@ -25,17 +25,40 @@ Set Implicit Arguments.
          _::_ = _::_
 *)
 
-Lemma app_split {X} (l1 l2 r1 r2 : list X) : 
+Lemma cons_inv {X} (x y : X) l m : x::l = y::m -> x = y /\ l = m.
+Proof.
+  inversion 1; auto.
+Qed.
+
+Lemma app_split X (l1 l2 r1 r2 : list X) :
           l1++r1 = l2++r2
        -> { m | l2 = l1++m /\ r1 = m++r2 }
         + { m | l1 = l2++m /\ r2 = m++r1 }.
 Proof.
-  revert l2; induction l1 as [ | x l1 IHl1 ].
-  + admit.
+  revert l2. 
+  induction l1 as [ | x l1 IHl1 ].
+  + intros l2 E.
+    left.
+    (* constructor 1 with l2. *)
+    exists l2.
+    simpl in *.
+    auto.
   + intros [ | y l2 ] E.
-    * admit.
-    * admit.
-Admitted.
+    * right. 
+      exists (x::l1).
+      auto.
+    * simpl in E |- *.
+      (* injection E; clear E; intros E <-. *)
+      (* inversion E. *)
+      destruct (@cons_inv X) with (1 := E) as [ <- E2 ].
+      apply IHl1 in E2.
+      destruct E2 as [ (m & H1 & H2) | (m & H1 & H2) ].
+      - left.
+        exists m.
+        subst. 
+        auto.
+      - right; exists m; subst; auto.
+Qed.
 
 (* Specialization of the previous result when r1 and r2 
    are of the form = _::_ 
@@ -52,7 +75,25 @@ Lemma app_cons_split {X} (x y : X) l1 l2 r1 r2 :
         + { l1 = l2 /\ x = y /\ r1 = r2 }
         + { m | l1 = l2++y::m /\ r2 = m++x::r1 }.
 Proof.
-Admitted.
+  intros E.
+  apply app_split in E.
+  simpl in E.
+  destruct E as [ (m & H1 & H2) | (m & H1 & H2) ].
+  + destruct m as [ | z m ]; simpl in H2; apply cons_inv in H2.
+    * destruct H2 as [ -> -> ].
+      rewrite <- app_nil_end in H1.
+      auto.
+    * destruct H2 as [ <- -> ].
+      do 2 left.
+      exists m; auto.
+  + destruct m as [ | z m ]; simpl in H2; apply cons_inv in H2.
+    * destruct H2 as [ -> -> ].
+      rewrite <- app_nil_end in H1.
+      auto.
+    * destruct H2 as [ <- -> ].
+      right.
+      exists m; auto.
+Qed.
 
 Reserved Notation "x ~p y" (at level 70, no associativity).
 Reserved Notation "x ~' y" (at level 70, no associativity).
@@ -65,29 +106,50 @@ Section perm.
       of a permutation *)
 
   Inductive perm : list X -> list X -> Prop :=
+
     | perm_nil   : nil ~p nil
+
     | perm_skip  : forall x l m, l ~p m -> x::l ~p x::m
+
     | perm_swap  : forall x y l, x::y::l ~p y::x::l
+
     | perm_trans : forall l m k, l ~p m -> m ~p k -> l ~p k
+
   where "l ~p m" := (perm l m).
 
   (* hint: induction on h *)
 
   Lemma perm_mskip h l m : l ~p m -> h++l ~p h++m.
   Proof.
-  Admitted.
+    (* induction h; simpl; intros; trivial; constructor; auto. *)
+    induction h as [ | x h IH ]; intros H; simpl.
+    + trivial.
+    + constructor.
+      (* apply IH, H. *)
+      auto.
+  Qed.
 
   (* hint: l++nil ~p l++nil *)
 
   Lemma perm_refl l : l ~p l.
   Proof.
-  Admitted.
+   (* induction l; constructor; auto. *)
+    rewrite (app_nil_end l).
+    apply perm_mskip.
+    constructor.
+  Qed.
 
   (* hint: induction on l *)
 
   Lemma perm_middle x l r : x::l++r ~p l++x::r.
   Proof.
-  Admitted.
+    induction l as [ | y l IH ]; simpl.
+    + apply perm_refl.
+    + apply perm_trans with (y :: x :: l ++ r).
+      * apply perm_swap.
+      * apply perm_skip.
+        assumption.
+  Qed.
 
   Hint Resolve perm_refl perm_skip perm_middle perm_mskip perm_swap : core.
 
@@ -95,14 +157,25 @@ Section perm.
 
   Lemma perm_comm l m : l++m ~p m++l.
   Proof.
-  Admitted.
+    revert m; induction l as [ | x l IHl ]; intros m; simpl.
+    + rewrite <- app_nil_end; auto.
+    + apply perm_trans with (l++x::m); auto.
+      Check perm_trans.
+      apply perm_trans with (1 := IHl _); simpl.
+      auto.
+  Qed.
 
   (* hint: induction on the proof of l ~p m *)
 
   Lemma perm_sym l m : l ~p m -> m ~p l.
   Proof.
-    induction 1 as [ | x l m H1 IH1 | x y l | l m k H1 IH1 H2 IH2 ].
-  Admitted.
+    intros H.
+    induction H as [ | x l m H1 IH1 | x y l | l m k H1 IH1 H2 IH2 ].
+    + apply perm_nil.
+    + apply perm_skip; auto.
+    + apply perm_swap.
+    + apply perm_trans with m; trivial. 
+  Qed.
 
   (* hints: 
       1) induction on l ~p m and 
@@ -110,12 +183,23 @@ Section perm.
    *)
 
   Lemma perm_nil_inversion l m : l ~p m -> m = nil -> l = nil.
-  Proof. Admitted.
+  Proof.
+    (* induction 1; auto; discriminate. *)
+    induction 1 as [ | x l m H1 IH1 | x y l | l m k H1 IH1 H2 IH2 ].
+    + trivial.
+    + discriminate.
+    + discriminate.
+    + auto.
+  Qed.
 
   (* use perm_nil_inversion *)
 
   Lemma perm_nil_inv l : l ~p nil -> l = nil.
-  Proof. Admitted.
+  Proof.
+    intros E.
+    apply perm_nil_inversion with (1 := E).
+    trivial.
+  Qed.
 
   Hint Resolve incl_refl incl_cons incl_tl : core.
 
@@ -124,7 +208,22 @@ Section perm.
 
   Lemma perm_incl l m : l ~p m -> incl l m.
   Proof.
-  Admitted.
+    unfold incl.
+    (* induction 1; auto; firstorder. *)
+    induction 1 as [ | x l m H1 IH1 | x y l | l m k H1 IH1 H2 IH2 ]; intros a; simpl.
+    + auto.
+    + firstorder.
+    + tauto.
+    + auto.
+  Qed.
+
+  Lemma perm_eq l m : l ~p m -> incl l m /\ incl m l.
+  Proof.
+    intros H.
+    split.
+    + apply perm_incl; trivial.
+    + apply perm_incl, perm_sym; trivial.
+  Qed.
 
   (** This is the hard lemma to 
       1) find out
@@ -152,37 +251,70 @@ Section perm.
            -> l1++l2 ~p m1++m2.
   Proof.
     induction 1 as [ | x l m H1 IH1 | x y l | l k m H1 IH1 H2 IH2 ]; intros l1 l2 m1 m2 El Em.
-    + admit.
+    + destruct l1; discriminate.
     + destruct l1 as [ | b l1 ]; destruct m1 as [ | c m1 ]; simpl in *;
         inversion El; inversion Em; subst; auto.
-      * admit.
-      * admit.
+      * apply perm_trans with (1 := H1), perm_sym.
+        apply perm_middle.
+      * apply perm_trans with (2 := H1); auto.
     + destruct l1 as [ | b1 [  | b2 l1 ] ];
       destruct m1 as [ | c1 [ | c2 m1 ] ];
       inversion El; inversion Em; subst; simpl; auto.
-      1-3: admit.
+      * apply perm_trans with (2 := perm_swap _ _ _).
+        apply perm_skip, perm_sym, perm_middle.
+      * apply perm_trans with (1 := perm_swap _ _ _); auto.
+      * apply app_cons_split in H5.
+        destruct H5 as [ [ (m & -> & ->) | (-> & _ & ->) ] | (m & -> & ->) ].
+        - rewrite app_ass; simpl.
+          apply perm_trans with (1 := perm_swap _ _ _).
+          do 2 apply perm_skip.
+          apply perm_mskip.
+          apply perm_sym; auto.
+        - apply perm_swap.
+        - rewrite app_ass; simpl.
+          apply perm_trans with (1 := perm_swap _ _ _).
+          do 2 apply perm_skip.
+          apply perm_mskip.
+          apply perm_sym; auto.
     + assert (In a k) as H3.
-      { admit. }
+      { apply perm_incl with (1 := H1).
+        subst.
+        apply in_app_iff; simpl; auto. }
       apply in_split in H3; destruct H3 as (k1 & k2 & Ek).
-      admit.
-  Admitted.
+      apply perm_trans with (k1++k2).
+      * apply IH1; auto.
+      * apply IH2; auto.
+  Qed.
 
   (* is an instance of perm_add_inv *)
 
   Lemma perm_cons_inv x l m : x::l ~p x::m -> l ~p m.
   Proof.
-  Admitted.
+    intros H.
+    Check perm_add_inv.
+    apply perm_add_inv with (l1 := nil) (m1 := nil) (2 := eq_refl) (3 := eq_refl) in H.
+    simpl in H; trivial.
+  Qed.
 
   Hint Resolve perm_cons_inv : core.
 
   Lemma perm_app_inv_l k l m : k++l ~p k++m -> l ~p m.
-  Proof. Admitted.
+  Proof.
+    induction k as [ | x k IHk ]; simpl.
+    + trivial.
+    + intros H; apply IHk.
+      revert H; apply perm_cons_inv.
+  Qed.
 
   (* hint: use perm_sym, perm_trans and perm_app_inv *)
  
   Lemma perm_app_inv_r l m k : l++k ~p m++k -> l ~p m.
   Proof.
-  Admitted.
+    intros H.
+    apply (perm_app_inv_l k).
+    apply perm_trans with (1 := perm_comm _ _), 
+          perm_trans with (1 := H); auto.
+  Qed.
 
   Hint Resolve perm_app_inv_l perm_app_inv_r : core.
 
@@ -192,8 +324,14 @@ Section perm.
    *)
 
   Lemma perm_length l m : l ~p m -> length l = length m.
-  Proof. 
-  Admitted.
+  Proof.
+    induction 1; simpl; lia. 
+(*    induction 1 as [ | x l m H1 IH1 | x y l | l k m H1 IH1 H2 IH2 ]; simpl.
+    + trivial.
+    + f_equal; trivial.
+    + trivial.
+    + transitivity (length k); auto. *)
+  Qed.
 
   (* hints: use perm_length (with the generalize tactic) 
             to eliminate the non-singleton cases and 
@@ -203,15 +341,42 @@ Section perm.
   Proof.
     intros H.
     generalize (perm_length H); simpl.
-    destruct l as [ | y [ | z l ] ]; simpl.
-  Admitted.
+    destruct l as [ | y [ | z l ] ]; simpl; try discriminate.
+    intros _; f_equal.
+    apply perm_incl in H.
+    unfold incl in H.
+    specialize (H y); simpl in H.
+    destruct H as [ -> | [] ]; auto.
+  Qed.
 
   (* hints: use perm_length to eliminate lists of length <> 2
             and then perm_[cons,sg]_inv *)
 
   Lemma perm_pair_inv l x y : l ~p x::y::nil -> l = x::y::nil \/ l = y::x::nil.
   Proof.
-  Admitted.
+    intros H.
+    generalize (perm_length H); simpl.
+    destruct l as [ | a [ | b [ | c l ] ] ]; simpl; try discriminate.
+    intros _.
+    assert (a = x \/ a = y) as E.
+    { apply perm_incl in H.
+      generalize (H a); simpl.
+      intros [ -> | [ -> | [] ] ]; auto. }
+    destruct E as [ -> | -> ].
+    + apply perm_cons_inv, perm_sg_inv in H.
+      left; f_equal; trivial.
+    + assert (y :: b :: nil ~p y :: x :: nil) as H1.
+      { apply perm_trans with (1 := H); auto. }
+      apply perm_cons_inv, perm_sg_inv in H1.
+      right; f_equal; trivial.
+  Qed.
+
+(*
+  Lemma perm_triple_inv l x y z : l ~p x::y::z::nil -> l = x::y::z::nil
+                                                    \/ ...
+  Proof.
+
+*)
 
   (* hints: use perm_sg_inv and "inversion" tactic *)
 
